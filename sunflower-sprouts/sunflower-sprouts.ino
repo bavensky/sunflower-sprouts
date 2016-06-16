9442a9bf62d82f1e42e0c369ab16e748b9e462c8
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-06-16 17:07:40 +0700

    no message

--- sunflower-sprouts/sunflower-sprouts.ino
+++ sunflower-sprouts/sunflower-sprouts.ino
@@ -2,9 +2,9 @@
 #include "DHT.h"
 #include <SPI.h>
 #include <SD.h>
-#include <TimeLib.h>
+//#include <TimeLib.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+//#include <WiFiUdp.h>
 
 File datalog;
 
@@ -24,14 +24,14 @@ File datalog;
 const char* ssid     = "@ESPertAP_001";
 const char* password = "espertap";
 
-static const char ntpServerName[] = "th.pool.ntp.org";
-const int timeZone = +7;
-WiFiUDP Udp;
-unsigned int localPort = 8888;
-time_t getNtpTime();
-void digitalClockDisplay();
-void printDigits(int digits);
-void sendNTPpacket(IPAddress &address);
+//static const char ntpServerName[] = "th.pool.ntp.org";
+//const int timeZone = +7;
+//WiFiUDP Udp;
+//unsigned int localPort = 8888;
+//time_t getNtpTime();
+//void digitalClockDisplay();
+//void printDigits(int digits);
+//void sendNTPpacket(IPAddress &address);
 
 unsigned int moi;
 
@@ -47,10 +47,10 @@ void setup() {
   Serial.begin(115200);
   delay(10);
 
-  if (!SD.begin(2)) {
-    Serial.println("initialization failed!");
-    return;
-  }
+//  if (!SD.begin(2)) {
+//    Serial.println("initialization failed!");
+//    return;
+//  }
 
   pinMode(DHTPIN, INPUT_PULLUP);
   delay(100);
@@ -62,22 +62,22 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.print("IP number assigned by DHCP is ");
-  Serial.println(WiFi.localIP());
-  Serial.println("Starting UDP");
-  Udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(Udp.localPort());
-  Serial.println("waiting for sync");
-  setSyncProvider(getNtpTime);
-  setSyncInterval(300);
+//  Serial.print("IP number assigned by DHCP is ");
+//  Serial.println(WiFi.localIP());
+//  Serial.println("Starting UDP");
+//  Udp.begin(localPort);
+//  Serial.print("Local port: ");
+//  Serial.println(Udp.localPort());
+//  Serial.println("waiting for sync");
+//  setSyncProvider(getNtpTime);
+//  setSyncInterval(300);
 
-  datalog = SD.open("datalog.csv", FILE_WRITE);
-  if (datalog) {
-    datalog.println("Date, Time, Temp, Humid, Moi");
-    datalog.close();
-    delay(100);
-  }
+//  datalog = SD.open("datalog.csv", FILE_WRITE);
+//  if (datalog) {
+//    datalog.println("Date, Time, Temp, Humid, Moi");
+//    datalog.close();
+//    delay(100);
+//  }
 }
 
 time_t prevDisplay = 0;
@@ -86,34 +86,34 @@ void loop() {
   static float t_dht;
   static float h_dht;
   moi = map(analogRead(A0), 600, 800, 100, 0);
-  datalog = SD.open("datalog.csv", FILE_WRITE);
-  if (datalog) {
+//  datalog = SD.open("datalog.csv", FILE_WRITE);
+//  if (datalog) {
     readDht(dht, &t_dht, &h_dht);
-    uploadThingsSpeak(t_dht, h_dht, moi);
-    datalog.print(day());
-    datalog.print("/");
-    datalog.print(month());
-    datalog.print("/");
-    datalog.print(year());
-    datalog.print(",");
-    datalog.print(hour());
-    datalog.print(":");
-    datalog.print(minute());
-    datalog.print(":");
-    datalog.print(second());
-    datalog.print(",");
-    datalog.print(t_dht);
-    datalog.print(",");
-    datalog.print(h_dht);
-    datalog.print(",");
-    datalog.println(moi);
-    datalog.close();
+//    uploadThingsSpeak(t_dht, h_dht, moi);
+//    datalog.print(day());
+//    datalog.print("/");
+//    datalog.print(month());
+//    datalog.print("/");
+//    datalog.print(year());
+//    datalog.print(",");
+//    datalog.print(hour());
+//    datalog.print(":");
+//    datalog.print(minute());
+//    datalog.print(":");
+//    datalog.print(second());
+//    datalog.print(",");
+//    datalog.print(t_dht);
+//    datalog.print(",");
+//    datalog.print(h_dht);
+//    datalog.print(",");
+//    datalog.println(moi);
+//    datalog.close();
 
-    delay(20 * 1000);
+//    delay(20 * 1000);
     reconnectWifiIfLinkDown();
-    digitalClockDisplay();
+//    digitalClockDisplay();
     Serial.println("done.");
-  }
+//  }
 }
 
 void reconnectWifiIfLinkDown() {
@@ -236,85 +236,85 @@ void readDht(DHT * dht, float * temp, float * humid) {
   *humid = f;
 }
 
-void digitalClockDisplay()
-{
-  // digital clock display of the time
-  Serial.print(hour());
-  printDigits(minute());
-  printDigits(second());
-  Serial.print(" ");
-  Serial.print(day());
-  Serial.print(".");
-  Serial.print(month());
-  Serial.print(".");
-  Serial.print(year());
-  Serial.println();
-}
+//void digitalClockDisplay()
+//{
+//  // digital clock display of the time
+//  Serial.print(hour());
+//  printDigits(minute());
+//  printDigits(second());
+//  Serial.print(" ");
+//  Serial.print(day());
+//  Serial.print(".");
+//  Serial.print(month());
+//  Serial.print(".");
+//  Serial.print(year());
+//  Serial.println();
+//}
 
-void printDigits(int digits)
-{
-  // utility for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
-  if (digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
+//void printDigits(int digits)
+//{
+//  // utility for digital clock display: prints preceding colon and leading 0
+//  Serial.print(":");
+//  if (digits < 10)
+//    Serial.print('0');
+//  Serial.print(digits);
+//}
 
 /*-------- NTP code ----------*/
 
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+//const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+//byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
 
-time_t getNtpTime()
-{
-  IPAddress ntpServerIP; // NTP server's ip address
-
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  // get a random server from the pool
-  WiFi.hostByName(ntpServerName, ntpServerIP);
-  Serial.print(ntpServerName);
-  Serial.print(": ");
-  Serial.println(ntpServerIP);
-  sendNTPpacket(ntpServerIP);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
-    }
-  }
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
-}
+//time_t getNtpTime()
+//{
+//  IPAddress ntpServerIP; // NTP server's ip address
+//
+//  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+//  Serial.println("Transmit NTP Request");
+//  // get a random server from the pool
+//  WiFi.hostByName(ntpServerName, ntpServerIP);
+//  Serial.print(ntpServerName);
+//  Serial.print(": ");
+//  Serial.println(ntpServerIP);
+//  sendNTPpacket(ntpServerIP);
+//  uint32_t beginWait = millis();
+//  while (millis() - beginWait < 1500) {
+//    int size = Udp.parsePacket();
+//    if (size >= NTP_PACKET_SIZE) {
+//      Serial.println("Receive NTP Response");
+//      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+//      unsigned long secsSince1900;
+//      // convert four bytes starting at location 40 to a long integer
+//      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+//      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+//      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+//      secsSince1900 |= (unsigned long)packetBuffer[43];
+//      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+//    }
+//  }
+//  Serial.println("No NTP Response :-(");
+//  return 0; // return 0 if unable to get the time
+//}
 
 // send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress & address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12] = 49;
-  packetBuffer[13] = 0x4E;
-  packetBuffer[14] = 49;
-  packetBuffer[15] = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
-}
+//void sendNTPpacket(IPAddress & address)
+//{
+//  // set all bytes in the buffer to 0
+//  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+//  // Initialize values needed to form NTP request
+//  // (see URL above for details on the packets)
+//  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+//  packetBuffer[1] = 0;     // Stratum, or type of clock
+//  packetBuffer[2] = 6;     // Polling Interval
+//  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+//  // 8 bytes of zero for Root Delay & Root Dispersion
+//  packetBuffer[12] = 49;
+//  packetBuffer[13] = 0x4E;
+//  packetBuffer[14] = 49;
+//  packetBuffer[15] = 52;
+//  // all NTP fields have been given values, now
+//  // you can send a packet requesting a timestamp:
+//  Udp.beginPacket(address, 123); //NTP requests are to port 123
+//  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+//  Udp.endPacket();
+//}
